983d4a7d57fd4dd08b01ccccaf058949414f31d4
Wilhelm Steins <wilhelmsteins@gmail.com>
2024-03-21 11:06:30 -0300

    project init

---
+++ src/main/java/com/springboot/motora/MotoraApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.motora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoraApplication.class, args);
+	}
+
+}
